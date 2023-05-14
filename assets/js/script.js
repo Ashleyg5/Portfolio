2e5ae202e357c3e3cf3b2589a072127c7fd0ef80
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-05-13 20:08:13 -0400

    added event ease project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var dateonadime = document.querySelector(`.surfimage`);
 
 dateonadime.addEventListener("click", function(){
+    location.href = `https://event-ease.herokuapp.com/`;
+})
+
+var dateonadime = document.querySelector(`.ledimage`);
+
+dateonadime.addEventListener("click", function(){
     location.href = `https://jamesgit22.github.io/Date-on-a-Dime/`;
 })
